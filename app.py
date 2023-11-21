f685b8b9134ed283ac53fb62c8cb1a9e56e36c40
Tiago Sanches da Silva <tiago.eem@gmail.com>
2023-11-21 09:14:35 -0300

    bug

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ async def handler(request):
 
 cpus_count = os.cpu_count()
 workers = cpus_count * 2 + 1
-app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000)), debug=bool(os.environ.get('DEBUG', ''), access_log=False, workers=int(os.environ.get('WEB_CONCURRENCY', 1))
+app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000)), debug=bool(os.environ.get('DEBUG', '')), access_log=False, workers=int(os.environ.get('WEB_CONCURRENCY', 1))
