25dec9098dc25ed2026c3810d2aa5670dad4bc5b
Tiago Sanches da Silva <tiago.eem@gmail.com>
2023-11-21 09:18:06 -0300

    if name

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from sanic import Sanic
 from sanic.response import json
 import os
 
-app = Sanic(__name__)
+app = Sanic("app")
 
 @app.get("/")
 async def handler(request):
@@ -10,4 +10,6 @@ async def handler(request):
 
 cpus_count = os.cpu_count()
 workers = cpus_count * 2 + 1
-app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000)), debug=bool(os.environ.get('DEBUG', '')), access_log=False, workers=int(os.environ.get('WEB_CONCURRENCY', 1)))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000)), debug=bool(os.environ.get('DEBUG', '')), access_log=False, workers=int(os.environ.get('WEB_CONCURRENCY', 1)))
