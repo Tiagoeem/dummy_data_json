e61fe5e6473136d635a44a3045fdad7f77c8f3c7
Tiago Sanches da Silva <tiago.eem@gmail.com>
2023-11-21 09:23:17 -0300

    teste

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ cpus_count = os.cpu_count()
 workers = cpus_count * 2 + 1
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000)), debug=bool(os.environ.get('DEBUG', '')), access_log=False, workers=int(os.environ.get('WEB_CONCURRENCY', 1)))
+    app.run(host="0.0.0.0", port=8000, debug=bool(os.environ.get('DEBUG', '')), access_log=False, workers=int(os.environ.get('WEB_CONCURRENCY', 1)))
